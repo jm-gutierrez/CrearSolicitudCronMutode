f1ff175b29a6787030f7564a1f80b766bdc6f6a5
Johanna Gutiérrez Meza <31225028+jm-gutierrez@users.noreply.github.com>
2019-11-21 03:05:49 -0500

    Update Cron.py

--- Cron.py
+++ Cron.py
@@ -8,10 +8,16 @@ from SQSConnection import SQSConnection
 from threading import Thread
 
 
-def execute_test(script):
+def execute_test(git_url):
     print('Se ejecuta la prueba')
-    txt = script
+    subprocess.call(['cd', '/home/ubuntu'])
+    subprocess.call(['git', 'clone', git_url])
 
+    suffix = git_url.index('.git')
+    last_slash = git_url.rindex('/') + 1
+    path = git_url[last_slash:suffix]
+
+    subprocess.call(['cd', path])
     output = subprocess.call(['mutode', '-c', '1', './'])
     if output < 0:
         print('error en ejecucion de prueba')
@@ -26,9 +32,9 @@ def process():
                 message_body = sqs_connection.message.get('Body')
                 msg = json.loads(message_body)
                 #Aqui va la conversion del json
-                script = msg['script']
+                git_url = msg['git']
                 sqs_connection.delete()
-                execute_test(script)
+                execute_test(git_url)
                 # if Settings.EMAIL_SEND == 'Y':
                 #     Email.send_email(email=email, tittle=tittle, name=user_first_name)
 
